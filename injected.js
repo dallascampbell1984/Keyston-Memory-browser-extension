a2573f068ccca2c1ba4f431eeaf837c740213f1d
dallascampbell1984 <42760766+dallascampbell1984@users.noreply.github.com>
2025-02-16 14:05:29 -0700

    Delete injected.js
    Not currently in use.

--- injected.js
+++
@@ -1,9 +0,0 @@
-console.log("🛠️ Injected script running inside webpage.");
-
-window.addEventListener("message", (event) => {
-    if (event.source !== window || !event.data || event.data.type !== "FROM_PAGE") return;
-
-    chrome.runtime.sendMessage(event.data.message, function(response) {
-        window.postMessage({ type: "FROM_EXTENSION", response }, "*");
-    });
-});
